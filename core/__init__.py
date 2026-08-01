a7e0b922384048ccce7c463b8596aed2bb18cf35
Tikhon Shabanov <tihon0392@gmail.com>
2026-08-01 14:01:14 +0300

    Removed synthesizer import from core init for server compatibility

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
+# core/__init__.py
 from .interval_calculator import Note, IntervalCalculator
 from .constants import NoteName, Interval, Direction, PAUSE, END_OF_SENTENCE
-from .synthesizer import Synthesizer
+# synthesizer не импортируем на сервере — требует sounddevice
